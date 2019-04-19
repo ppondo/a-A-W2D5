85e51ff2360d618bfc4862c591f5be5217b6c448
ppondo <patrickpondo7@gmail.com>
2019-04-19 14:27:43 -0400

    Complete Phase2 hashing

--- MyHashMap/lib/p02_hashing.rb
+++ MyHashMap/lib/p02_hashing.rb
@@ -4,11 +4,27 @@ end
 
 class Array
   def hash
+    hash = 0
+    self.each_with_index do |el, i|
+      hash = hash^(el.hash * i)
+    end
+
+    hash
   end
 end
 
 class String
   def hash
+    alpha = ('a'..'z').to_a
+    hash = 0
+    spl = self.split('')
+
+    spl.each_with_index do |el, i|
+      idx = alpha.index(el)
+      hash = hash^(idx.hash * i)
+    end
+
+    hash
   end
 end
 
@@ -16,6 +32,12 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
+    hash = 0
+    self.each do |k,v|
+      hash += k.hash + v.hash
+    end
+
+    hash
     0
   end
 end
