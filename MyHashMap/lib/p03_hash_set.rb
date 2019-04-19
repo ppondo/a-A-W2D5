bff63f3b5d60b90227cf04e777cca96cdc01a818
ppondo <patrickpondo7@gmail.com>
2019-04-19 14:40:35 -0400

    Finish HashSet class

--- MyHashMap/lib/p03_hash_set.rb
+++ MyHashMap/lib/p03_hash_set.rb
@@ -7,12 +7,29 @@ class HashSet
   end
 
   def insert(key)
+    resize! if count == num_buckets
+    hashed = key.hash
+
+    unless include?(key)
+      @store[hashed % num_buckets] << key 
+      @count += 1
+      return true
+    end
   end
 
   def include?(key)
+    hashed = key.hash
+    @store[hashed % num_buckets].include?(key)
   end
 
   def remove(key)
+    hashed = key.hash
+    if include?(key)
+      bucket = @store[hashed % num_buckets]
+      key_i = bucket.index(key)
+      bucket.delete_at(key_i)
+      @count -= 1
+    end
   end
 
   private
@@ -26,5 +43,17 @@ class HashSet
   end
 
   def resize!
+    keys = []
+    
+    @store.each do |bucket|
+      bucket.each do |key|
+        keys << key
+        self.remove(key) 
+      end
+    end
+
+    num_buckets.times { @store << [] }
+
+    keys.each { |key| self.insert(key) }
   end
 end
